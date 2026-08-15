c02c007330c0c58941b13c627cfb00d9c72eef30
0Dex <163450531+MSR-Cracker@users.noreply.github.com>
2026-08-15 19:43:15 +0300

    Clean up java/src/com/dex2c/pipeline/NativeNames.java

--- java/src/com/dex2c/pipeline/NativeNames.java
+++
@@ -1,9 +0,0 @@
-package com.dex2c.pipeline;
-
-final class NativeNames {
-    static final String MODULE = "msr";
-    static final String LIB_FILE = "lib" + MODULE + ".so";
-
-    private NativeNames() {
-    }
-}
